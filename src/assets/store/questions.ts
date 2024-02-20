4f24e039e32bfada90f1f7cd24016cae213114fd
MiguelCagnate <miguelcbarreneche@gmail.com>
2024-02-20 18:54:50 +0100

    changen data json place

--- src/assets/store/questions.ts
+++ src/assets/store/questions.ts
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+/* import { create } from "zustand";
 import { type Question } from "./types";
 import confetti from "canvas-confetti";
 import { persist, devtools } from "zustand/middleware";
@@ -59,6 +59,94 @@ export const useQuestionsStore = create<State>()(
               userSelectedAnswer: answerIndex,
             };
             // actualizamos el estado
+            set({ questions: newQuestions }, false, "SELECT_ANSWER");
+          },
+
+          goNextQuestion: () => {
+            const { currentQuestion, questions } = get();
+            const nextQuestion = currentQuestion + 1;
+
+            if (nextQuestion < questions.length) {
+              set({ currentQuestion: nextQuestion }, false, "GO_NEXT_QUESTION");
+            }
+          },
+
+          goPreviousQuestion: () => {
+            const { currentQuestion } = get();
+            const previousQuestion = currentQuestion - 1;
+
+            if (previousQuestion >= 0) {
+              set(
+                { currentQuestion: previousQuestion },
+                false,
+                "GO_PREVIOUS_QUESTION"
+              );
+            }
+          },
+
+          reset: () => {
+            set({ currentQuestion: 0, questions: [] }, false, "RESET");
+          },
+        };
+      },
+      {
+        name: "questions",
+      }
+    )
+  )
+);
+ */
+
+import { create } from "zustand";
+import { type Question } from "./types";
+import confetti from "canvas-confetti";
+import { persist, devtools } from "zustand/middleware";
+import data from "../../data.json"; // Import local JSON data
+
+interface State {
+  questions: Question[];
+  currentQuestion: number;
+  fetchQuestions: (limit: number) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
+  goNextQuestion: () => void;
+  goPreviousQuestion: () => void;
+  reset: () => void;
+}
+
+export const useQuestionsStore = create<State>()(
+  devtools(
+    persist(
+      (set, get) => {
+        return {
+          loading: false,
+          questions: [],
+          currentQuestion: 0,
+
+          fetchQuestions: async (limit: number) => {
+            // Use the imported JSON data directly
+            const questions = data
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit);
+            set({ questions }, false, "FETCH_QUESTIONS");
+          },
+
+          selectAnswer: (questionId: number, answerIndex: number) => {
+            const { questions } = get();
+            const newQuestions = [...questions]; // Make a shallow copy
+            const questionIndex = newQuestions.findIndex(
+              (q) => q.id === questionId
+            );
+            const questionInfo = newQuestions[questionIndex];
+            const isCorrectUserAnswer =
+              questionInfo.correctAnswer === answerIndex;
+
+            if (isCorrectUserAnswer) confetti();
+
+            newQuestions[questionIndex] = {
+              ...questionInfo,
+              isCorrectUserAnswer,
+              userSelectedAnswer: answerIndex,
+            };
             set({ questions: newQuestions }, false, "SELECT_ANSWER");
           },
 
